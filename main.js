098f90d16d41cd9e9bd487a586107c05a9c3ca92
DENIS SAVGIR <denis@DENISs-MacBook-Pro.local>
2020-04-21 09:40:37 -0400

    Done

--- main.js
+++ main.js
@@ -2,6 +2,63 @@
  * YOUR CODE HERE *
  ******************/
 
+const Grade = function(assignment, score) {
+  return {
+    assignment: assignment,
+    score: score
+  };
+}
+
+const Term = function(hours, grades = []) {
+  return {
+    addGrade: function(assignment, score) {
+      //this.grades.push(Grade(assignment, grade));
+      this.grades.push({assignment: assignment, score: score});
+    },
+
+    hours: hours,
+    grades: grades
+  };
+}
+
+const Course = function(name, terms = []) {
+  return {
+    addTerm: function(hours) {
+      this.terms.push(Term(hours));
+    },
+    
+    name: name,
+    terms: terms
+  };
+}
+
+const Student = function(name, course, term = 1) {
+  return {
+    addCourse: function(name) {
+      this.courses.push(Course(name))
+    },
+    getAverage: function(courseName = "", termNum = "") {
+      let allGrades = [];
+      for(course of this.courses) {
+        if(courseName == "" || course.name == courseName) {
+          for(let i = 0; i < course.terms.length; i++) {
+            if(termNum == "" || i == termNum) {
+              for(grades of course.terms[i].grades) {
+                allGrades.push(grades.score);
+              }
+            }
+          }
+        }
+      }
+      return allGrades.reduce((total, num) => (total + num)) / allGrades.length;
+    },
+
+    name: name,
+    course: course,
+    term: term,
+    courses: []
+  }
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
